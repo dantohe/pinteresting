39931f36889da4cb438ba61d76e84ce1b9790650
daniel antohe <dantohe@gmail.com>
2015-05-22 07:48:35 -0600

    adding bootstrap

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,8 +9,8 @@
 //
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
-//
 //= require jquery
 //= require jquery_ujs
+//= require bootstrap
 //= require turbolinks
 //= require_tree .
